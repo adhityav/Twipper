8eaa875b41bfd70f45316865d344bc1502ba761d
Adhitya Vadivel <adhitya.vadivel@gmail.com>
2017-06-01 14:43:48 -0700

    Fixed minor errors

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -24,9 +24,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
 end
